bed06541e6021bd0c4dac52de19b30d6089f0727
Smitha Hosmani <smitha_hosmani@yahoo.com>
2019-02-25 22:37:44 -0700

    padded random with leading zeros

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,13 +1,15 @@
 class Keys
 
   def initialize
-
+    @key
   end
 
   def random_number
-    number = rand(0..99999).to_s
-    binding.pry
-    number.length
-
+    @key = rand(0..99999).to_s
+    if @key.length < 5
+      number = 5 - @key.length
+      @key.rjust(number,"0")
+    end
+    @key
   end
 end
