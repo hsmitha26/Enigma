3886b6fcb55b36ef02a804f739ac7d98f8bcea5d
Smitha Hosmani <smitha_hosmani@yahoo.com>
2019-02-25 22:20:37 -0700

    created a 5-digit random number and passed test for length

---
+++ lib/keys.rb
@@ -0,0 +1,13 @@
+class Keys
+
+  def initialize
+
+  end
+
+  def random_number
+    number = rand(0..99999).to_s
+    binding.pry
+    number.length
+
+  end
+end
--- test/keys_test.rb
+++ test/keys_test.rb
@@ -1,7 +1,17 @@
 require './test/test_helper'
-require 'keys'
+require './lib/keys'
 
-class KeysTest < Minitest::Test  
+class KeysTest < Minitest::Test
 
+  def setup
+    @key = Keys.new
+  end
 
+  def test_it_exists
+    assert_instance_of Keys, @key
+  end
+
+  def test_it_generates_five_digits_at_random
+    assert_equal 5, @key.random_number
+  end
 end
