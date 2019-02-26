250605c8392bd96adb816803396afa04bc42e45a
Smitha Hosmani <smitha_hosmani@yahoo.com>
2019-02-25 22:46:36 -0700

    passed instance test

--- test/offsets_test.rb
+++ test/offsets_test.rb
@@ -1,2 +1,14 @@
 require './test/test_helper'
 require './lib/offsets'
+
+class OffsetsTest < Minitest::Test
+
+  def setup
+    @off = Offset.new
+  end
+
+  def test_it_exists
+    assert_instance_of Offset, @off
+  end
+
+end
