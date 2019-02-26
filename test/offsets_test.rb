cf2f3e52969c039f45ab9378fad42ac6e646b70b
Smitha Hosmani <smitha_hosmani@yahoo.com>
2019-02-26 14:46:49 -0700

    passed tests for class and date length

--- test/offsets_test.rb
+++ test/offsets_test.rb
@@ -1,5 +1,6 @@
 require './test/test_helper'
 require './lib/offsets'
+require 'date'
 
 class OffsetsTest < Minitest::Test
 
@@ -11,4 +12,12 @@ class OffsetsTest < Minitest::Test
     assert_instance_of Offset, @off
   end
 
+  def test_date_length_is_six
+    @off.current_date
+    assert_equal 6, @off.date_length
+  end
+
+  def test_date_is_a_string
+    assert_equal String, @off.current_date.class
+  end
 end
