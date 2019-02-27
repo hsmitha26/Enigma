89caeb253c97ccb3fd46c4d8ea41ba9e4cb72d10
Smitha Hosmani <smitha_hosmani@yahoo.com>
2019-02-27 11:01:22 -0700

    passed encrypt test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,9 +2,33 @@ require './lib/shift'
 
 class Enigma
 
-  def message(word)
-    message_array = word.split(//)
-    message_array.each_slice(4).to_a
+  def initialize
+    @shift = Shift.new
+    @character_set = @shift.character_set
   end
 
+  def message(word)
+    message_array = word.split(//)
+  end
+
+  def encrypt(word, key, date)
+    message_array = message(word)
+    shift = @shift.shift_values(key, date)
+    translated = message_array.map do |letter|
+      if index = @character_set.index(letter)
+        replaced = @character_set.rotate(shift[0])[index]
+        shift = shift.rotate
+        replaced
+      else
+        replaced = letter
+        shift = shift.rotate
+        replaced
+      end
+    end.join
+    translation(translated, key, date)
+  end
+
+  def translation(message, key, date)
+    {encryption: message, key: key, date: date}
+  end
 end
